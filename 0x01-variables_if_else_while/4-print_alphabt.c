337d4f159f68c9ec3d76f9624da017faf4827e4d
Kaneki881 <133522919+Kaneki881@users.noreply.github.com>
2023-06-17 01:55:13 +0100

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,18 +2,19 @@
 
 /**
  * main - Prints the alphabet.
- *
+ *  description: 'I never thought that it would pay off'
  * Return: Always 0 (Success)
  */
-int main(void)
-{
-char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-	int i;
+int i;
 
-	for (i = 0; i < 52; i++)
+	for (i = 97; i < 123; i++)
 	{
-		putchar(alp[i]);
+		if (i != 101 && i != 113)
+		{
+			putchar(i);
+		}
 	}
 	putchar('\n');
 	return (0);
 }
+
