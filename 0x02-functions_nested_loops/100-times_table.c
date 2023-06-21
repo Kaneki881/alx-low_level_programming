0f3eac62e35b86fc968c9fcccc301bff27385e77
Kaneki881 <133522919+Kaneki881@users.noreply.github.com>
2023-06-21 03:33:10 +0100

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,35 +6,39 @@
  */
 void print_times_table(int n)
 {
-	int i, j, oper;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
 		for (i = 0; i <= n; i++)
 		{
-			_putchar(48);
-			for (j = 1; j <= n; j++)
+			for (j = 0; j <= n; j++)
 			{
-				oper = i * j;
-				_putchar(44);
-				_putchar(32);
-				if (oper <= 9)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar(32);
-					_putchar(32);
-					_putchar(oper + 48);
-				}
-				else if (oper <= 99)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
-					_putchar(32);
-					_putchar((oper / 10) + 48);
-					_putchar((oper % 10) + 48);
-				}
-				else
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
-					_putchar(((oper / 100) % 10) + 48);
-					_putchar(((oper / 10) % 10) + 48);
-					_putchar((oper % 10) + 48);
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
