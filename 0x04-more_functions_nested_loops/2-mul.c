a394e11ab9b008becf4adf6450e10457bfb8e616
Amin <alarrouchi@gmail.com>
2023-06-22 17:33:31 -0700

    more function and loops in C

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul-checks for checks for a digit (0 through 9).
+ * @a: variable
+ * @b: variable
+ * return 0 always
+ */
+int mul(int a, int b)
+{
+	int R;
+
+	R = a * b;
+	return (R);
+}
+
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+void print_most_numbers(void)
+{
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i);
+		}
+	}
+	_putchar("\n");
+}
