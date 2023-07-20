3a57fc09560a8de33565aeef29767919aa5a1a34
Amin <alarrouchi@gmail.com>
2023-07-20 16:02:19 -0700

    today we have Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,5 +1,6 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
 * print_strings - a function that prints strings, followed by a new line.
 * @separator: the string to be printed between strings.
